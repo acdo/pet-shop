b48545a07b0d0773b77309278a39d93b605108d7
Alexander Do <acd@seas.upenn.edu>
2019-11-19 15:34:56 -0500

    Office hours

--- contracts/Adoption.sol
+++ contracts/Adoption.sol
@@ -2,14 +2,15 @@ pragma solidity ^0.5.0;
 
 contract Adoption {
 	address[16] public adopters;
-	uint[16] public prices;
+	//uint[16] public prices;
 
 	// Adopting a pet
 	function adopt(uint petId) public payable returns (uint) {
 		require(petId >= 0 && petId <= 15);
 
 		adopters[petId] = msg.sender;
-		prices[petId] = msg.value;
+		//prices[petId] = msg.value;
+		
 
 		return petId;
 	}
